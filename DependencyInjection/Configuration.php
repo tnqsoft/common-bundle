ae5ea3ae54d11a58cabe2a638c71438cf9011878
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2016-07-14 15:52:21 +0700

    Change config dependcy.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tnqsoft_common');
+        $rootNode = $treeBuilder->root('tnq_soft_common');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
